4b0ad87e99805c4ce38c1a8c15d49e138288193f
huiyan1 <57057229+huiyan1@users.noreply.github.com>
2022-01-02 16:44:01 +0800

    Update test.m

--- code/test.m
+++ code/test.m
@@ -1,8 +1,8 @@
 clear all
 close all
 clc
-IN1= imread('original_images/pair1_1.tif');
-IN2= imread('original_images/pair1_2.tif');
+IN1= imread('source_images/pair1_1.tif');
+IN2= imread('source_images/pair1_2.tif');
 % IN1=rgb2gray(IN1);
 % IN2=rgb2gray(IN2);
 I1=im2double(IN1);
